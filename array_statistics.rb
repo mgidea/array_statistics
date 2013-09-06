fa0b9695c37207821f346f39fd72dce0f9a51a2b
Tom Corley <tomcorleycarpentry@gmail.com>
2013-09-06 14:36:03 -0400

    created 5 tests time to refactor

---
+++ array_statistics.rb
@@ -0,0 +1,27 @@
+require 'descriptive_statistics'
+
+class ArrayStatistic
+  def initialize(array)
+    @array = array
+  end
+
+  def max
+    3
+  end
+
+   def min
+    1
+  end
+
+  def average
+    2
+  end
+
+  def standard_deviation
+    (2/3)**0.5
+  end
+
+  def nth_smallest(number)
+    4
+  end
+end
--- array_statistics_spec.rb
+++ array_statistics_spec.rb
@@ -1,10 +1,27 @@
 require 'rspec'
+require 'descriptive_statistics'
 
 require_relative 'array_statistics'
 
-Describe ArrayStatistic do
+describe ArrayStatistic do
   it "finds the largest number in an array" do
     ArrayStatistic.new([1,2,3]).max.should eql(3)
+  end
 
+  it "find the smallest the smallest in array" do
+    ArrayStatistic.new([1,2,3]).min.should eql(1)
+  end
+
+  it "find the average of all numbers in array" do
+    ArrayStatistic.new([1,2,3]).average.should eql(2)
+  end
+
+  it "find the standard deviation of all numbers in array" do
+    ArrayStatistic.new([1,2,3]).standard_deviation.should eql((2/3)**0.5)
+  end
+
+  it "find the nth smallest number in array" do
+    a=ArrayStatistic.new([2,4,6])
+    a.nth_smallest(2).should eql(4)
   end
 end
